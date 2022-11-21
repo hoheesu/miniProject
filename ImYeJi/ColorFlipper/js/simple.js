ef5ebf3eeb6302755618fc828aeade0cf0e1048a
1myeji <yimyg0596@naver.com>
2022-11-21 21:38:34 +0900

    feat: finish project

--- ImYeJi/ColorFlipper/js/simple.js
+++ ImYeJi/ColorFlipper/js/simple.js
@@ -2,10 +2,11 @@
 const btnEl = document.querySelector('.btn')
 const colorEl = document.querySelector('.color')
 const body = document.querySelector('body')
+const color = ['Red', '#F15025', 'Green', 'Rgba(133,122,200)']
 
 // 버튼 클릭시 배경색 변경
 btnEl.addEventListener('click', function() {
-  let colorCode  = "#" + Math.round(Math.random() * 0xffffff).toString(16)
-  colorEl.innerHTML = colorCode
-  body.style.backgroundColor = colorCode
+  let i  = Math.floor(Math.random() * color.length)
+  colorEl.innerHTML = color[i]
+  body.style.backgroundColor = color[i]
 })
