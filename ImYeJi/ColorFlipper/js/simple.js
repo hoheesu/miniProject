bec81af1331cd6ed77f8aca9b1d414693e0c70eb
1myeji <yimyg0596@naver.com>
2022-11-21 22:17:45 +0900

    fix: change simple color array

--- ImYeJi/ColorFlipper/js/simple.js
+++ ImYeJi/ColorFlipper/js/simple.js
@@ -2,11 +2,12 @@
 const btnEl = document.querySelector('.btn')
 const colorEl = document.querySelector('.color')
 const body = document.querySelector('body')
-const color = ['Red', '#F15025', 'Green', 'Rgba(133,122,200)']
+const color = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Navy', 'Blueviolet']
 
-// 버튼 클릭시 배경색 변경
+// 버튼(simple) 클릭시 배경색 변경
 btnEl.addEventListener('click', function() {
   let i  = Math.floor(Math.random() * color.length)
   colorEl.innerHTML = color[i]
   body.style.backgroundColor = color[i]
 })
+
