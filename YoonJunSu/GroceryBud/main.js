d8a37bc72008cc82bb53745a6da4a242d3b4ab85
hoheesu <hoheesu@naver.com>
2022-11-25 20:30:24 +0900

    feat: Finish todo project

--- YoonJunSu/GroceryBud/main.js
+++ YoonJunSu/GroceryBud/main.js
@@ -1,8 +1,17 @@
+import Sortable from 'sortablejs'
+
 const addData = document.querySelector('.list-data')
 const addBtn = document.querySelector('.add-btn')
 const removeAllBtn = document.querySelector('button.remove-all')
 const listUl = document.querySelector('.list-main')
 const removeBtn = document.querySelectorAll('.remove')
+
+const editModal = document.querySelector('.modal')
+const editInput = document.querySelector('.modal > .edit-value')
+const editClose = document.querySelector('.modal > .close')
+const editSubmit = document.querySelector('.modal > .submit')
+const backColor = document.querySelector('.bgc')
+
 let localIndex = 0 // index를 쌓는 변수
 
 renderLocal()
@@ -12,7 +21,7 @@ addBtn.addEventListener('click', (e) => {
   e.preventDefault()
   renderList(addData.value) // renderList() 매개변수에 value값을 넣어준다.
   addData.value = ''
-  findListIndex() // ul.list-main>li 를 모두 찾아주는 함수를 작동시킨다.
+  findList() // ul.list-main>li 를 모두 찾아주는 함수를 작동시킨다.
 })
 // list 전체 삭제 이벤트
 removeAllBtn.addEventListener('click', (e) => {
@@ -40,32 +49,81 @@ function renderLocal (){
     ls.setItem(localIndex,i[1]) //arr을 순회하면서 로컬에 저장을 해주고
     localIndex++ // localIndex값을 증가
   }
-  findListIndex() // ul.list-main>li 를 모두 찾아주는 함수를 작동시킨다.
+  findList() // ul.list-main>li 를 모두 찾아주는 함수를 작동시킨다.
 }
 // 리스트를 추가하는 함수
 function renderList (list) {
   const createLi = document.createElement('li') //li태그를 만들고 
   createLi.innerHTML = `
   <p>${list}</p>
-  <button class="edit">eidt</button>
-  <button class="remove">remove</button>`
+  <button class="edit"><i class="fa-solid fa-pen-to-square"></i></button>
+  <button class="remove"><i class="fa-solid fa-trash"></i></button>`
   listUl.append(createLi)
   localStorage.setItem(localIndex,list)
   localIndex++
 }
 
-function findListIndex() {
+function findList() {
   let removeBtn = document.querySelectorAll('ul.list-main > li > button.remove')
-  let findListIndex = document.querySelectorAll('ul.list-main > li')  
+  let editBtn = document.querySelectorAll('ul.list-main > li > button.edit')
+  let findListIndex = document.querySelectorAll('ul.list-main > li')
+
   removeBtn.forEach((e,index) => { // button.remove 을 모두 순회하고 분류
     e.onclick = () => { // 클릭이 된 button.remove가 있다면
     findListIndex[index].remove() // 브라우저 화면상에서 li의 인덱스를 찾아서 지운다.
     window.localStorage.removeItem(index) // 로컬스토리지 내에서도 index키값을 지운다.
     }
   })
+
+  editBtn.forEach((e,index) => { // button.remove 을 모두 순회하고 분류
+    e.onclick = () => { // 클릭이 된 button.remove가 있다면
+      let listP = findListIndex[index].querySelector('p') // index번째<li>의 <p>
+      editBack('visible')
+      editInput.focus()
+      editInput.value = listP.textContent // modal>.edit-valu=의 value값에 <p>의 내용으로 넣어준다.
+      editClose.addEventListener('click', () => {
+        editBack('hidden')
+      })
+      editSubmit.addEventListener('click', (e)=>{
+        e.preventDefault()
+        listP.innerHTML = editInput.value // submit버튼을 누르면 <p>에 .edit-valu=의 value값을 넣는다.
+        window.localStorage.setItem(index,editInput.value) // 로컬스토리지에 index번호와 value를 넣어준다.
+        editBack('hidden')
+      })
+    }
+  })
+
+  function editBack(visibility) {
+    editModal.style.visibility = visibility
+    backColor.style.visibility = visibility
+  }
 }
 
 
+let el = document.getElementById('items');
 
+let sortable = new Sortable(el, {
+  animation: 150,
+  ghostClass: 'blue-background-class',
+  onEnd: function (event){
+    console.log(event)
+    dragDrop()
+  }
+})
 
-
+function dragDrop () {
+  let arr = []
+  let findListIndex = document.querySelectorAll('ul.list-main > li')
+  findListIndex.forEach((e,index)=>{
+    let listP = findListIndex[index].querySelector('p')
+    arr.push([index,listP.textContent])
+  })
+  let ls = window.localStorage
+  ls.clear() // 로컬스토리지를 비운다.
+  localIndex = 0 // localIndex를 초기화 시키고
+  for(const i of arr){ 
+    ls.setItem(localIndex,i[1]) //arr을 순회하면서 로컬에 저장을 해주고
+    localIndex++ // localIndex값을 증가
+  }
+  findList() // ul.list-main>li 를 모두 찾아주는 함수를 작동시킨다.
+}
